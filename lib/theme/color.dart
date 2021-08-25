c93d718252604e71e9beb1741bb34b9acc9a3197
Bintaaaa <bijantium.yt.32@gmail.com>
2021-08-25 23:02:02 +0700

    menambahkan directory theme

---
+++ lib/theme/color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color purpleColor = Color(0xFF5843BE);
+Color orangeColor = Color(0xFFFF9376);
+Color blackColor = Color(0xFF000000);
+Color whiteColor = Color(0xFFFFFFFF);
+Color greyColor = Color(0xFF82868E);
---
+++ lib/theme/font.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:ui_bwa_koskos/theme/color.dart';
+
+var blackText = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: blackColor,
+);
+var whiteText = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: whiteColor,
+);
+var greyText = GoogleFonts.poppins(
+  fontWeight: FontWeight.w300,
+  color: greyText,
+);
+var purpleText = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: purpleText,
+);
