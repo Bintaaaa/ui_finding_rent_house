8db026302ef5d7def53f6542defb0d47f270fef5
Bintaaaa <bijantium.yt.32@gmail.com>
2021-09-02 22:52:37 +0700

    show dialog alert

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -33,6 +33,39 @@ class _DetailPageState extends State<DetailPage> {
         );
       }
     }
+    Future<void> showConfirmation() async{
+      return showDialog(
+        context: context,
+        builder: (BuildContext context){
+          return AlertDialog(
+            title: const Text('Konfirmasi'),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: const <Widget>[
+                  Text('Apakah kamu mau menghubungi pemilik kost?'),
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: const Text('Batal'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              TextButton(
+                child: const Text('Hubungi'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  launchUrl('tel://${widget.space.phone}');
+                },
+              ),
+            ],
+          );
+        }
+      );
+    }
+
     return Scaffold(
       backgroundColor: whiteColor,
       body: SafeArea(
@@ -245,7 +278,7 @@ class _DetailPageState extends State<DetailPage> {
                         width: MediaQuery.of(context).size.width - (2*edge),
                         child: RaisedButton(
                           onPressed: (){
-                            launchUrl('tel://${widget.space.phone}');
+                            showConfirmation();
                           },
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(17),
